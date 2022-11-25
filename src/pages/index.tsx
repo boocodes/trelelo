f092762ab3321f1fd27aa83100fca7f616d6bfcf
Nick Felker <felker.nick@bk.ru>
2022-11-25 15:34:22 +0600

    init personal page

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import React from "react";
+
+
+
+interface Props{
+
+}
+
+function PersonalPage(props:Props) {
+    return(
+        <ExternalWrapper>
+
+            
+        </ExternalWrapper>
+    )
+}
+
+
+
+const ExternalWrapper = styled.div`
+
+`
+
+
+export default PersonalPage;
